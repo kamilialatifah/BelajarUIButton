84611d101dd3bab87a3dfda24dbc07eac3d4c025
Kamilia Latifah <kamilia.latifah@gmail.com>
2019-07-03 09:15:08 +0700

    Initial Commit

---
+++ BelajarUIButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarUIButton
+//
+//  Created by Kamilia Latifah on 03/07/19.
+//  Copyright © 2019 masaksendiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
